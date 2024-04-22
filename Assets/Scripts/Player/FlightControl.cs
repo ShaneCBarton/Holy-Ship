746b27841d711f2b22716721e32a17880e51e6d9
Shane Barton <shanecb@hotmail.ca>
2024-04-22 13:21:07 -0400

    Added ship rotation when a/d is pressed.

--- Assets/Scripts/Player/FlightControl.cs
+++ Assets/Scripts/Player/FlightControl.cs
@@ -1,8 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FlightControl : MonoBehaviour
 {
- 
+    [SerializeField] private float turnSpeed;
+
+    private PlayerControls playerControls;
+    private Rigidbody2D myRigidbody;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponentInChildren<Rigidbody2D>();
+        playerControls = new PlayerControls();
+    }
+
+    private void Start()
+    {
+        playerControls.Flight.Rotate.performed += _ => OnRotate();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void OnRotate()
+    {
+        float direction = playerControls.Flight.Rotate.ReadValue<float>();
+        myRigidbody.AddTorque(direction * turnSpeed);
+        Debug.Log("Turning");
+    }
 }
